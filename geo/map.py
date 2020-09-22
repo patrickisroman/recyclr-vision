dbe1412b0780cc29586731dd89583065785fdfc1
patrickisroman <patrickec246@gmail.com>
2020-09-21 20:26:23 -0700

    Update map object

--- geo/map.py
+++ geo/map.py
@@ -1,31 +1,53 @@
-import os
-import geojson
+import geopandas
 import pathlib
 import glob
-
-from shapely.geometry.polygon import Polygon
+import random
 from shapely.geometry import Point
 
 fences_path = pathlib.Path('./fences/')
 
+'''
+We don't use projections for geofence calculations because with relatively small
+map sizes, error without projection is significantly below the margin of error
+for GPS localization. However, geopandas makes it relatively easy if needed.
+'''
+
 class Map():
 	def __init__(self, file_name):
+		self.gdf = self.__load_mapfile(file_name)
+		self.detections = []
+
+	def __load_mapfile(self, file_name):
 		path = pathlib.Path(file_name)
 		if not path.parent == fences_path:
 			path = fences_path.joinpath(path)
+
 		mapfiles = glob.glob(str(fences_path.joinpath('*.geojson')))
-		assert(str(path) in mapfiles)
+		assert str(path) in mapfiles, "Map file %s does not exist within path: %s" % (path, fences_path)
+		return geopandas.read_file(str(path))
 
-		with open(path, 'r') as f:
-			self.map = geojson.load(f)
-			self.poly = Polygon([Point(lat, lng) for lng,lat in self.map.geometry.coordinates[0]])
+	def inside(self, x, y=None):
+		if type(x) in (int, float):
+			return self.gdf.contains(Point(x, y)).bool()
+		elif type(x) is Point:
+			return self.gdf.contains(x)
 
-	def inside(self, point):
-		if type(point) is tuple:
-			point = Point(point)
-		if type(point) is list:
-			point = Point(tuple(point))
-		return self.poly.contains(point)
+	def generate_points(self, n=1, point=None, radius_meters=0, distribution:str='uniform'):
+		assert self.inside(point)
+		points = []
+
+		while len(points) < n:
+			new_point = Point(
+				point[0] + radius_meters * random.uniform(-1, 1),
+				point[1] + radius_meters * random.uniform(-1, 1)) 
+			if self.inside(new_point):
+				points.append(new_point)
+			
+		return points
 	
-	def point_inside(self, x, y):
-		return self.inside((x, y))
+	def detection_exists(self, point=None, radius_meters:int=0, val:int=0):
+		for p in self.detections:
+			if p.distance(point) <= radius_meters:
+				if p.val() == val:
+					return True
+		return False
